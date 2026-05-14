af96f6c6341c9cb55005c170d87136526b909f15
nierjulianpimentel15 <pimentel.nierjulian@gmail.com>
2026-05-14 08:40:41 +0800

    Delete Game_Modularized directory

--- Game_Modularized/include/hud.h
+++
@@ -1,9 +0,0 @@
-#ifndef HUD_H
-#define HUD_H
-
-#include "types.h"
-
-// Draws the level counter and key/portal status messages
-void DrawHUD(int currentLevel, bool hasKey);
-
-#endif // HUD_H
--- Game_Modularized/include/player.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "types.h"
-
-// Reads input and moves the player for one frame (dt = delta-time in seconds)
-void UpdatePlayer(Player *p, float dt);
-
-#endif // PLAYER_H
