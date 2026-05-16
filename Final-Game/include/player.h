6210ea5ce6128f4193e01ee9ae837d2bcc819dba
OkJustn <justinblancox3@gmail.com>
2026-05-16 14:39:29 +0800

    Delete Final-Game directory

--- Final-Game/include/player.h
+++
@@ -1,9 +0,0 @@
-#ifndef PLAYER_H
-#define PLAYER_H
-
-#include "types.h"
-
-// Reads mouse/keyboard input and moves the player; handles wall sliding
-void UpdatePlayer(Player *p, GameState *state, float dt);
-
-#endif // PLAYER_H
