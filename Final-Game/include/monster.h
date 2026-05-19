1360fa07eb028cdd082237305190a0117b79e5d6
OkJustn <justinblancox3@gmail.com>
2026-05-19 13:56:36 +0800

    Delete Final-Game directory

--- Final-Game/include/monster.h
+++
@@ -1,9 +0,0 @@
-#ifndef MONSTER_H
-#define MONSTER_H
-
-#include "types.h"
-
-// Moves the monster toward the player each frame
-void UpdateMonster(Monster *m, Player *p, GameState *state, float dt);
-
-#endif // MONSTER_H
